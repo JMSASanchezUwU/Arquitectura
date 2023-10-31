1b03e927b4de999636a4ec44036e312a4f9e261b
JMSASanchezUwU <106613839+JMSASanchezUwU@users.noreply.github.com>
2023-10-31 12:16:33 -0600

    telegram

--- servidor/index.js
+++ servidor/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
+const { bot } = require('./services/whatsapp')
 
 
 //Se crear el servidor
