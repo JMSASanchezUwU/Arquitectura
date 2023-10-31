e33ae70a4e4bb1cfa62c7cae0de6e805f3acb94d
Oscar Pérez <ossytres8@gmail.com>
2023-10-31 10:11:10 -0600

    Update Ventas.ts

--- SistemaVentas/src/app/Models/Ventas.ts
+++ SistemaVentas/src/app/Models/Ventas.ts
@@ -1,40 +1,61 @@
 export class Ventas {
     _id?: number;
-    nombreCliente: string;
-    emailCliente: string;
-    direccionCliente: string;
-    fechaCompra: Date;
-    estatus: string;
-    telefono: string;
-    total: number;
-    nombreProducto: string;
-    precio: number;
-    img: string;
-    // compraProducto: Array<{
-    //     nombreProducto: string;
-    //     precio: number;
-    //     img: string;
-    //   }>;
-  
-    constructor(nombreCliente: string, emailCliente: string,direccionCliente: string, fechaCompra: Date,  estatus: string,
-        telefono: string, total: number,  nombreProducto: string, precio: number, img: string
+    nombreCliente?: string;
+    emailCliente?: string;
+    direccionCliente?: string;
+    fechaCompra?: Date;
+    estatus?: string;
+    telefono?: string;
+    total?: number;
+    compraProducto: {
+      nombreProducto: string;
+      precio: number;
+      img: string;
+      cantidad:number;
+      subtotal?:number;
+    }[];
+    
+    constructor(
+       nombreCliente: string,
+       emailCliente: string,
+       direccionCliente: string,
+       fechaCompra: Date,
+       estatus: string,
+       telefono: string,
+       total: number,
+       compraProducto: { 
+        nombreProducto: string;
+        precio: number; 
+        img: string;
+        cantidad:number; }[]
+      ) {
+          this.nombreCliente = nombreCliente;
+          this.emailCliente = emailCliente;
+          this.direccionCliente = direccionCliente;
+          this.fechaCompra = fechaCompra;
+          this.estatus = estatus;
+          this.telefono = telefono;
+          this.total = total;
+          this.compraProducto = compraProducto;
+        }
+}
+
+
+
+    // this.nombreProducto = nombreProducto;
+      // this.precio = precio;
+      // this.img = img;
+// nombreProducto: string, precio: number, img: string, 
         //  compraProducto: Array<{
         //     nombreProducto: string;
         //     precio: number;
         //     img: string;
         //   }>  
-        ) {
-      this.nombreCliente = nombreCliente;
-      this.emailCliente = emailCliente;
-      this.direccionCliente = direccionCliente;
-      this.fechaCompra = fechaCompra;
-      this.estatus = estatus;
-      this.telefono = telefono;
-      this.total = total;
-      this.nombreProducto = nombreProducto;
-      this.precio = precio;
-      this.img = img;
-      // this.compraProducto = compraProducto;
-    }
-}
-
+            // nombreProducto: string;
+    // precio: number;
+    // img: string;
+    // compraProducto: Array<{
+    //     nombreProducto: string;
+    //     precio: number;
+    //     img: string;
+    //   }>;
